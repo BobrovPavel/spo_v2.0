a071835a1d26342cd935be86329e67f166f07a0f
pavel-bobrov <pavel.babrou@startmatter.com>
2019-12-09 18:10:12 +0300

    add file uploadin function

--- full_info.py
+++ full_info.py
@@ -1,44 +1,9 @@
-import platform
-import shutil
-import time
-
-import math
-import os
 import re
-
-import psutil
 import wmi
+import math
 import uptime
+import psutil
 
-# import wmi
-from pprint import pprint
-
-# print(platform.system())
-#
-# print(platform.version())
-#
-# print(platform.platform())
-#
-# print(platform.uname())
-#
-# print(platform.system())
-#
-# print(platform.processor())
-#
-# print(platform.machine())
-#
-# print(platform.architecture())
-# c = wmi.WMI()
-# c.Win32_ComputerSystem.methods.keys()
-
-# total, used, free = shutil.disk_usage("d:")
-#
-# print("Total: %s GB" % (total / math.pow(1024, 3)))
-# print("Used: %s GB" % (used / math.pow(1024, 3)))
-# print("Free: %s GB" % (free / 1024 ** 3))
-
-
-#
 dps = psutil.disk_partitions()
 c = wmi.WMI()
 proc = c.Win32_Processor()[0]
@@ -46,60 +11,75 @@ bios = c.Win32_Bios()[0]
 board = c.Win32_BaseBoard()[0]
 
 
-# print(disk.get_devices_file_system())
+def get_all_devices():
+    return [dp.device for dp in dps]
 
 
-class GetInfo:
-    def get_all_devices(self):
-        return [dp.device for dp in dps]
-
-    def get_devices_space(self):
-        tmp = {k: v for (k, v) in
-               zip(self.get_all_devices(), [psutil.disk_usage(device) for device in self.get_all_devices()])}
-        spaces = ["total", "used", "free", "percent"]
-        result = {}
-        for device in self.get_all_devices():
-            result[device] = {k: v for (k, v) in
-                              zip(spaces, [tmp.get(device).total, tmp.get(device).used, tmp.get(device).free,
-                                           tmp.get(device).percent])}
-        return result
-
-    def get_devices_file_system(self):
-        return {k: v for (k, v) in zip(self.get_all_devices(), (device.fstype for device in dps))}
-
-    def convert_to_gb(self, number):
-        return float('{:.3f}'.format(number / math.pow(1024, 3)))
-
-    def get_bios_info(self):
-        keys = [x.strip() for x in re.findall('(.*?)=', str(bios))]
-        values = [x.strip() for x in re.findall('=\s+([^\n]+)[\;]', str(bios))]
-        return {k: v for (k, v) in zip(keys, values)}
-
-    def get_cpu_info(self):
-        keys = [x.strip() for x in re.findall('(.*?)=', str(proc))]
-        values = [x.strip() for x in re.findall('=\s+([^\n]+)[\;]', str(proc))]
-        return {k: v for (k, v) in zip(keys, values)}
-
-    def get_board_info(self):
-        keys = [x.strip() for x in re.findall('(.*?)=', str(board))]
-        values = [x.strip() for x in re.findall('=\s+([^\n]+)[\;]', str(board))]
-        return {k: v for (k, v) in zip(keys, values)}
-
-    def get_time(self):
-        time = uptime.uptime()
-        days = time // (24 * 3600)
-        hours = (time - days * 3600 * 24) // 3600
-        minutes = (time - days * 3600 * 24 - hours * 3600) // 60
-        seconds = time - days * 3600 * 24 - hours * 3600 - minutes * 60
-        return {"Uptime": f"{int(days)} days, {int(hours)} hours, {int(minutes)} minutes, {seconds:.3f} seconds",
-                "BootTime": str(uptime.boottime())}
+def get_devices_space():
+    tmp = {k: v for (k, v) in
+           zip(get_all_devices(), [psutil.disk_usage(device) for device in get_all_devices()])}
+    spaces = ["total", "used", "free", "percent"]
+    result = {}
+    for device in get_all_devices():
+        result[device] = {k: v for (k, v) in
+                          zip(spaces, [tmp.get(device).total, tmp.get(device).used, tmp.get(device).free,
+                                       tmp.get(device).percent])}
+    return result
 
 
+def get_devices_file_system():
+    return {k: v for (k, v) in zip(get_all_devices(), (device.fstype for device in dps))}
 
 
+def convert_to_gb(number):
+    return float('{:.3f}'.format(number / math.pow(1024, 3)))
 
-info = GetInfo()
 
-print(info.get_time()["BootTime"])
-print(info.get_time()["Uptime"])
+def get_bios_info():
+    keys = [x.strip() for x in re.findall('(.*?)=', str(bios))]
+    values = [x.strip('"') for x in re.findall('=\s+([^\n]+)[\;]', str(bios))]
+    return {k: v for (k, v) in zip(keys, values)}
 
+
+def get_cpu_info():
+    keys = [x.strip() for x in re.findall('(.*?)=', str(proc))]
+    values = [x.strip('"') for x in re.findall('=\s+([^\n]+)[\;]', str(proc))]
+    return {k: v for (k, v) in zip(keys, values)}
+
+
+def get_board_info():
+    keys = [x.strip() for x in re.findall('(.*?)=', str(board))]
+    values = [x.strip('"') for x in re.findall('=\s+([^\n]+)[\;]', str(board))]
+    return {k: v for (k, v) in zip(keys, values)}
+
+
+def get_time():
+    time = uptime.uptime()
+    days = time // (24 * 3600)
+    hours = (time - days * 3600 * 24) // 3600
+    minutes = (time - days * 3600 * 24 - hours * 3600) // 60
+    seconds = time - days * 3600 * 24 - hours * 3600 - minutes * 60
+    return {"Uptime": f"{int(days)} days, {int(hours)} hours, {int(minutes)} minutes, {seconds:.3f} seconds",
+            "BootTime": str(uptime.boottime())}
+
+
+def create_report():
+    result = []
+    result.append("\n---------------DISK INFO---------------\n")
+    for k, v in get_devices_space().items():
+        result.append(f"{k}: {v} \n")
+    for k, v in get_devices_file_system().items():
+        result.append(f"{k}: {v} \n")
+    result.append("\n---------------BIOS INFO---------------\n")
+    for k, v in get_bios_info().items():
+        result.append(f"{k}: {v} \n")
+    result.append("\n---------------CPU INF---------------\n")
+    for k, v in get_cpu_info().items():
+        result.append(f"{k}: {v} \n")
+    result.append("\n---------------MOTHERBOARD INFO---------------\n")
+    for k, v in get_board_info().items():
+        result.append(f"{k}: {v} \n")
+    result.append("\n---------------TIME INFO---------------\n")
+    for k, v in get_time().items():
+        result.append(f"{k}: {v} \n")
+    return result
--- widget.py
+++ widget.py
@@ -1,87 +1,127 @@
-import datetime
+import os
 import sys
-
-from PyQt5.QtCore import QTimer
-from PyQt5 import QtCore
-
+import dropbox
+import datetime
 from full_info import *
+from PyQt5 import QtCore
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QTimer
 
 
-class tabdemo(QTabWidget):
+class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.info = GetInfo()
-
+        self.tabwidget = QTabWidget()
         self.resize(1000, 900)
         self.tab1 = QWidget()
         self.setStyleSheet("font-size: 18px;")
-
         self.tab2 = QWidget()
         self.tab3 = QWidget()
         self.tab4 = QWidget()
         self.tab5 = QWidget()
-        self.time_label = QLabel(info.get_time().get("Uptime"))
-
-        self.addTab(self.tab1, "Disk Info")
-        self.addTab(self.tab2, "BIOS Info")
-        self.addTab(self.tab3, "CPU Info")
-        self.addTab(self.tab4, "Board Info")
-        self.addTab(self.tab5, "Time Info")
-
+        self.uptime_time_label = QLabel()
+        self.tabwidget.addTab(self.tab1, "Disk Info")
+        self.tabwidget.addTab(self.tab2, "BIOS Info")
+        self.tabwidget.addTab(self.tab3, "CPU Info")
+        self.tabwidget.addTab(self.tab4, "Board Info")
+        self.tabwidget.addTab(self.tab5, "Time Info")
         self.show_tabs()
         self.show_time()
-        self.setWindowTitle("tab demo")
+        self.show_tool_bar()
+        self.setWindowTitle("Spo Project")
+        self.setWindowIcon(QIcon("images/icon.png"))
+        self.setCentralWidget(self.tabwidget)
+
+    def on_download_click(self):
+        with open("report.txt", "w") as file:
+            for i in create_report():
+                file.write(i)
+
+    def on_upload_click(self):
+        self.on_download_click()
+        access_token = 'qdk5zENbrdAAAAAAAAAAFvnjG-29PQguW8DRsghUQJFT5t9o5_6lcXPquzqcePtU'
+        file_from = 'report.txt'  # local file path
+        file_to = f'/spo_project/{datetime.datetime.now()}report.txt'  # dropbox path
+        dbx = dropbox.Dropbox(access_token)
+        f = open(file_from, 'rb')
+        dbx.files_upload(f.read(), file_to)
+
+        # Ссыдка на файлы
+        # https://www.dropbox.com/sh/rovxw54epmu6pp4/AADD4fe28-j58p6NswVWOM9ia?dl=0
+
+    def show_tool_bar(self):
+        exitAction = QAction(QIcon("images/exit.png"), "Exit", self)
+        exitAction.setShortcut("Ctrl+Q")
+        exitAction.triggered.connect(qApp.quit)
+        download_report = QAction(QIcon("images/download.png"), "Download report", self)
+        download_report.setShortcut("Ctrl+D")
+        download_report.triggered.connect(self.on_download_click)
+        upload_report = QAction(QIcon("images/upload.png"), "Download report", self)
+        upload_report.setShortcut("Ctrl+D")
+        upload_report.triggered.connect(self.on_upload_click)
+        self.toolbar = self.addToolBar("Exit")
+        self.toolbar.addAction(exitAction)
+        self.toolbar.addAction(download_report)
+        self.toolbar.addAction(upload_report)
 
     def show_tabs(self):
         self.grid_layout = QGridLayout()
-        self.tab1.setLayout(self.grid_layout)
         self.grid_layout.setSpacing(0)
+        self.tab1.setLayout(self.grid_layout)
         self.show_disk_table_headers(self.grid_layout)
         self.show_disk_form(self.grid_layout)
-
-        self.draw_tab_layout(self.info.get_bios_info().items())
+        self.draw_tab_layout(get_bios_info().items())
         self.tab2.setLayout(self.horizontallayout)
-        self.draw_tab_layout(self.info.get_cpu_info().items())
+        self.draw_tab_layout(get_cpu_info().items())
         self.tab3.setLayout(self.horizontallayout)
-        self.draw_tab_layout(self.info.get_board_info().items())
+        self.draw_tab_layout(get_board_info().items())
         self.tab4.setLayout(self.horizontallayout)
 
-        # self.draw_tab_layout(self.info.get_time().items())
-        # self.tab5.setLayout(self.horizontallayout)
-
     def draw_tab_layout(self, data):
-        self.verticalLayout = QVBoxLayout()
+        self.verticallayout = QVBoxLayout()
         self.verticalLayout_2 = QVBoxLayout()
         for k, v in data:
             label = QLabel(k)
             label.setStyleSheet("max-height: 20px; font-size: 17px;")
-            self.verticalLayout.addWidget(label)
+            self.verticallayout.addWidget(label)
             label_2 = QLabel(v)
             label_2.setStyleSheet("max-height: 20px; font-size: 17px;")
             self.verticalLayout_2.addWidget(label_2)
         last_label = QLabel()
-        self.verticalLayout.addWidget(last_label)
+        self.verticallayout.addWidget(last_label)
         self.verticalLayout_2.addWidget(last_label)
         self.horizontallayout = QHBoxLayout()
-        self.horizontallayout.addLayout(self.verticalLayout)
+        self.horizontallayout.addLayout(self.verticallayout)
         self.horizontallayout.addLayout(self.verticalLayout_2)
         self.horizontallayout.setSpacing(1)
         self.tab4.setLayout(self.horizontallayout)
 
     def show_time(self):
-        self.verticalLayout = QVBoxLayout()
-        self.label = QLabel("Curent Ttime")
-        self.verticalLayout.addWidget(self.label, 0, QtCore.Qt.AlignHCenter | QtCore.Qt.AlignTop)
+        self.layout = QVBoxLayout()
+        self.label_2 = QLabel("Current Uptime")
+        self.layout.addWidget(self.label_2, 0, QtCore.Qt.AlignHCenter)
+        self.layout.addWidget(self.uptime_time_label, 0, QtCore.Qt.AlignHCenter)
+        self.uptime_time_label.setStyleSheet("max-height: 45px; font-size: 40px; margin-bottom: 40px;")
+        self.label_2.setStyleSheet("max-height: 25px; margin-top: 20px;")
+        self.label = QLabel("Current Time")
+        self.label.setStyleSheet("max-height: 25px;")
+        self.layout.addWidget(self.label, 0, QtCore.Qt.AlignHCenter)
         self.current_time_label = QLabel(str(datetime.datetime.today()))
-        self.verticalLayout.addWidget(self.current_time_label, 0, QtCore.Qt.AlignHCenter | QtCore.Qt.AlignTop)
-        self.label = QLabel("Curent Uptime")
-        self.verticalLayout.addWidget(self.label, 0, QtCore.Qt.AlignHCenter | QtCore.Qt.AlignTop)
-        self.verticalLayout.addWidget(self.time_label, 0, QtCore.Qt.AlignHCenter | QtCore.Qt.AlignTop)
-        self.tab5.setLayout(self.verticalLayout)
+        self.current_time_label.setStyleSheet("max-height: 45px; font-size: 40px; margin-bottom: 40px;")
+        self.layout.addWidget(self.current_time_label, 0, QtCore.Qt.AlignHCenter)
+        self.label_1 = QLabel("Last BootTime")
+        self.label_1.setStyleSheet("max-height: 25px;")
+        self.layout.addWidget(self.label_1, 0, QtCore.Qt.AlignHCenter)
+        self.boot_time_label = QLabel(get_time().get("BootTime"))
+        self.boot_time_label.setStyleSheet("max-height: 45px; font-size: 40px; margin-bottom: 40px;")
+        self.layout.addWidget(self.boot_time_label, 0, QtCore.Qt.AlignHCenter)
+        self.last_label = QLabel()
+        self.layout.addWidget(self.last_label)
+        self.tab5.setLayout(self.layout)
 
     def update_time(self):
-        self.time_label.setText(self.info.get_time().get("Uptime"))
+        self.uptime_time_label.setText(get_time().get("Uptime"))
         self.current_time_label.setText(str(datetime.datetime.today()))
 
     def show_disk_table_headers(self, layout):
@@ -89,7 +129,7 @@ class tabdemo(QTabWidget):
         position = 2
         for header in headers:
             title = QLabel(header)
-            title.setStyleSheet("font-size:18px; max-height: 30px")
+            title.setStyleSheet("font-size:18px; max-height: 30px;")
             layout.addWidget(title, 0, position)
             position += 1
 
@@ -100,21 +140,23 @@ class tabdemo(QTabWidget):
 
     def show_disk_form(self, layout):
         string_number = 1
-        for disk in self.info.get_all_devices():
+        for disk in get_all_devices():
             label = QLabel(disk)
-            label.setStyleSheet("font-size: 20px;")
+            label.setStyleSheet("font-size: 20px; max-height: 30px;")
             layout.addWidget(label, string_number, 1)
-            self.draw_disk_info_row(string_number, 2, str(self.info.get_devices_space().get(disk).get("total")), layout)
-            self.draw_disk_info_row(string_number, 3, str(self.info.get_devices_space().get(disk).get("used")), layout)
-            self.draw_disk_info_row(string_number, 4, str(self.info.get_devices_space().get(disk).get("free")), layout)
-            self.draw_disk_info_row(string_number, 5, str(self.info.get_devices_space().get(disk).get("percent")), layout)
-            self.draw_disk_info_row(string_number, 6, str(self.info.get_devices_file_system().get(disk)), layout)
+            self.draw_disk_info_row(string_number, 2, str(get_devices_space().get(disk).get("total")), layout)
+            self.draw_disk_info_row(string_number, 3, str(get_devices_space().get(disk).get("used")), layout)
+            self.draw_disk_info_row(string_number, 4, str(get_devices_space().get(disk).get("free")), layout)
+            self.draw_disk_info_row(string_number, 5, str(get_devices_space().get(disk).get("percent")), layout)
+            self.draw_disk_info_row(string_number, 6, str(get_devices_file_system().get(disk)), layout)
             string_number += 1
+        last_label = QLabel()
+        layout.addWidget(last_label, string_number, 1)
 
 
 def main():
     app = QApplication(sys.argv)
-    ex = tabdemo()
+    ex = MyWidget()
     ex.show()
     timer = QTimer()
     timer.timeout.connect(ex.update_time)
