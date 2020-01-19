abc74e35b96bdecbc24bd4acfa2591011c7ff959
BobrovPavel <pavel.babrou@startmatter.com>
2020-01-19 22:41:03 +0300

    add users info and gpu info, fix permissions crash

--- full_info.py
+++ full_info.py
@@ -1,4 +1,6 @@
 import re
+from pprint import pprint
+
 import wmi
 import math
 import uptime
@@ -6,9 +8,16 @@ import psutil
 
 dps = psutil.disk_partitions()
 c = wmi.WMI()
-proc = c.Win32_Processor()[0]
+cpu = c.Win32_Processor()[0]
+gpu = c.Win32_VideoController()[0]
 bios = c.Win32_Bios()[0]
 board = c.Win32_BaseBoard()[0]
+users = c.Win32_UserAccount()
+
+wmi_monitor = c.Win32_DesktopMonitor()[0]
+wmi_keyboard = c.Win32_Keyboard()[0]
+wmi_mouse = c.Win32_PnpEntity()[0]
+wmi_networkAdapter = c.Win32_NetworkAdapter()[0]
 
 
 def get_all_devices():
@@ -16,14 +25,17 @@ def get_all_devices():
 
 
 def get_devices_space():
-    tmp = {k: v for (k, v) in
-           zip(get_all_devices(), [psutil.disk_usage(device) for device in get_all_devices()])}
-    spaces = ["total", "used", "free", "percent"]
     result = {}
-    for device in get_all_devices():
-        result[device] = {k: v for (k, v) in
-                          zip(spaces, [tmp.get(device).total, tmp.get(device).used, tmp.get(device).free,
-                                       tmp.get(device).percent])}
+    try:
+        tmp = {k: v for (k, v) in
+               zip(get_all_devices(), [psutil.disk_usage(device) for device in get_all_devices()])}
+        spaces = ["total", "used", "free", "percent"]
+        for device in get_all_devices():
+            result[device] = {k: v for (k, v) in
+                              zip(spaces, [tmp.get(device).total, tmp.get(device).used, tmp.get(device).free,
+                                           tmp.get(device).percent])}
+    except:
+        print("Failed to get disk info. Check permissions.")
     return result
 
 
@@ -42,8 +54,14 @@ def get_bios_info():
 
 
 def get_cpu_info():
-    keys = [x.strip() for x in re.findall('(.*?)=', str(proc))]
-    values = [x.strip('"') for x in re.findall('=\s+([^\n]+)[\;]', str(proc))]
+    keys = [x.strip() for x in re.findall('(.*?)=', str(cpu))]
+    values = [x.strip('"') for x in re.findall('=\s+([^\n]+)[\;]', str(cpu))]
+    return {k: v for (k, v) in zip(keys, values)}
+
+
+def get_gpu_info():
+    keys = [x.strip() for x in re.findall('(.*?)=', str(gpu))]
+    values = [x.strip('"') for x in re.findall('=\s+([^\n]+)[\;]', str(gpu))]
     return {k: v for (k, v) in zip(keys, values)}
 
 
@@ -61,6 +79,37 @@ def get_time():
     seconds = time - days * 3600 * 24 - hours * 3600 - minutes * 60
     return {"Uptime": f"{int(days)} days, {int(hours)} hours, {int(minutes)} minutes, {seconds:.3f} seconds",
             "BootTime": str(uptime.boottime())}
+
+
+def get_users():
+    user_list = {}
+    for user in users:
+        keys = [x.strip() for x in re.findall('(.*?)=', str(user))]
+        values = [x.strip('"') for x in re.findall('=\s+([^\n]+)[\;]', str(user))]
+        name = user.name
+        user_list[name] = {k: v for (k, v) in zip(keys, values)}
+    return user_list
+
+
+def get_devices_info():
+    devices = {}
+    keys = [x.strip() for x in re.findall('(.*?)=', str(wmi_monitor))]
+    values = [x.strip('"') for x in re.findall('=\s+([^\n]+)[\;]', str(wmi_monitor))]
+    monitor = {k: v for (k, v) in zip(keys, values)}
+    keys = [x.strip() for x in re.findall('(.*?)=', str(wmi_keyboard))]
+    values = [x.strip('"') for x in re.findall('=\s+([^\n]+)[\;]', str(wmi_keyboard))]
+    keyboard = {k: v for (k, v) in zip(keys, values)}
+    keys = [x.strip() for x in re.findall('(.*?)=', str(wmi_mouse))]
+    values = [x.strip('"') for x in re.findall('=\s+([^\n]+)[\;]', str(wmi_mouse))]
+    mouse = {k: v for (k, v) in zip(keys, values)}
+    keys = [x.strip() for x in re.findall('(.*?)=', str(wmi_networkAdapter))]
+    values = [x.strip('"') for x in re.findall('=\s+([^\n]+)[\;]', str(wmi_networkAdapter))]
+    network_adapter = {k: v for (k, v) in zip(keys, values)}
+    devices["Monitor"] = monitor
+    devices["Keyboard"] = keyboard
+    devices["Mouse"] = mouse
+    devices["Network Adapter"] = network_adapter
+    return devices
 
 
 def create_report():
@@ -83,3 +132,13 @@ def create_report():
     for k, v in get_time().items():
         result.append(f"{k}: {v} \n")
     return result
+
+
+# print(c.Win32_DesktopMonitor()[0])
+# print(c.Win32_Keyboard()[0])
+# print(c.Win32_PnpEntity()[0])
+# print(c.Win32_NetworkAdapter()[0])
+
+# print(get_devices_info())
+
+print(bios)
--- widget.py
+++ widget.py
@@ -13,24 +13,43 @@ class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         self.tabwidget = QTabWidget()
-        self.resize(1000, 900)
+        self.resize(1000, 500)
         self.tab1 = QWidget()
         self.setStyleSheet("font-size: 18px;")
         self.tab2 = QWidget()
         self.tab3 = QWidget()
         self.tab4 = QWidget()
         self.tab5 = QWidget()
+        self.tab6 = QWidget()
+        self.tab7 = QWidget()
+
+        self.cpu_tab = QScrollArea()
+        self.cpu_tab.setWidget(self.tab3)
+        self.cpu_tab.setWidgetResizable(True)
+
+        self.gpu_tab = QScrollArea()
+        self.gpu_tab.setWidget(self.tab6)
+        self.gpu_tab.setWidgetResizable(True)
+
+        self.users_tab = QScrollArea()
+        self.users_tab.setWidget(self.tab7)
+        self.users_tab.setWidgetResizable(True)
+
         self.uptime_time_label = QLabel()
         self.tabwidget.addTab(self.tab1, "Disk Info")
         self.tabwidget.addTab(self.tab2, "BIOS Info")
-        self.tabwidget.addTab(self.tab3, "CPU Info")
+        self.tabwidget.addTab(self.cpu_tab, "CPU Info")
+        self.tabwidget.addTab(self.gpu_tab, "GPU Info")
         self.tabwidget.addTab(self.tab4, "Board Info")
+        self.tabwidget.addTab(self.users_tab, "Users")
         self.tabwidget.addTab(self.tab5, "Time Info")
         self.show_tabs()
         self.show_time()
+        self.show_users_info()
         self.show_tool_bar()
         self.setWindowTitle("Spo Project")
         self.setWindowIcon(QIcon("images/icon.png"))
+
         self.setCentralWidget(self.tabwidget)
 
     def on_download_click(self):
@@ -38,14 +57,27 @@ class MyWidget(QMainWindow):
             for i in create_report():
                 file.write(i)
 
+    # Back up the reference to the exceptionhook
+    sys._excepthook = sys.excepthook
+
+    def my_exception_hook(exctype, value, traceback):
+        # Print the error and traceback
+        print(exctype, value, traceback)
+        # Call the normal Exception hook after
+        sys._excepthook(exctype, value, traceback)
+        sys.exit(1)
+
+    # Set the exception hook to our wrapping function
+    sys.excepthook = my_exception_hook
+
     def on_upload_click(self):
         self.on_download_click()
-        access_token = 'qdk5zENbrdAAAAAAAAAAFvnjG-29PQguW8DRsghUQJFT5t9o5_6lcXPquzqcePtU'
+        access_token = 'qdk5zENbrdAAAAAAAAAAHb9BTmYECI_QwjBy3-m7uqzfBysP52ereRZEKqnRkqvU'
         file_from = 'report.txt'  # local file path
         file_to = f'/spo_project/{datetime.datetime.now()}report.txt'  # dropbox path
         dbx = dropbox.Dropbox(access_token)
-        f = open(file_from, 'rb')
-        dbx.files_upload(f.read(), file_to)
+        file = open(file_from, 'rb')
+        dbx.files_upload(file.read(), file_to)
 
         # Ссыдка на файлы
         # https://www.dropbox.com/sh/rovxw54epmu6pp4/AADD4fe28-j58p6NswVWOM9ia?dl=0
@@ -57,7 +89,7 @@ class MyWidget(QMainWindow):
         download_report = QAction(QIcon("images/download.png"), "Download report", self)
         download_report.setShortcut("Ctrl+D")
         download_report.triggered.connect(self.on_download_click)
-        upload_report = QAction(QIcon("images/upload.png"), "Download report", self)
+        upload_report = QAction(QIcon("images/upload.png"), "Upload report to cloud", self)
         upload_report.setShortcut("Ctrl+D")
         upload_report.triggered.connect(self.on_upload_click)
         self.toolbar = self.addToolBar("Exit")
@@ -74,6 +106,8 @@ class MyWidget(QMainWindow):
         self.draw_tab_layout(get_bios_info().items())
         self.tab2.setLayout(self.horizontallayout)
         self.draw_tab_layout(get_cpu_info().items())
+        self.tab6.setLayout(self.horizontallayout)
+        self.draw_tab_layout(get_gpu_info().items())
         self.tab3.setLayout(self.horizontallayout)
         self.draw_tab_layout(get_board_info().items())
         self.tab4.setLayout(self.horizontallayout)
@@ -83,19 +117,49 @@ class MyWidget(QMainWindow):
         self.verticalLayout_2 = QVBoxLayout()
         for k, v in data:
             label = QLabel(k)
-            label.setStyleSheet("max-height: 20px; font-size: 17px;")
+            label.setStyleSheet("max-height: 20px; font-size: 17px; max-width: 350px;")
             self.verticallayout.addWidget(label)
             label_2 = QLabel(v)
-            label_2.setStyleSheet("max-height: 20px; font-size: 17px;")
+            label_2.setStyleSheet("max-height: 20px; font-size: 17px; max-width: 350px;")
             self.verticalLayout_2.addWidget(label_2)
+
         last_label = QLabel()
         self.verticallayout.addWidget(last_label)
         self.verticalLayout_2.addWidget(last_label)
         self.horizontallayout = QHBoxLayout()
         self.horizontallayout.addLayout(self.verticallayout)
         self.horizontallayout.addLayout(self.verticalLayout_2)
+
         self.horizontallayout.setSpacing(1)
         self.tab4.setLayout(self.horizontallayout)
+
+    def show_users_info(self):
+        self.layout = QVBoxLayout()
+        for key, value in get_users().items():
+            self.user_name_layout = QVBoxLayout()
+            self.qh = QHBoxLayout()
+            self.user_name_label = QLabel(key)
+            self.user_name_layout.addWidget(self.user_name_label)
+            self.user_name_label.setStyleSheet("max-height: 30px; font-size: 27px; margin: 20px 0 5px")
+            self.user_name_label.setAlignment(QtCore.Qt.AlignCenter)
+            self.layout.addLayout(self.user_name_layout)
+
+            self.user_key_layout = QVBoxLayout()
+            self.user_value_layout = QVBoxLayout()
+            self.user_name_layout.addLayout(self.qh)
+            self.qh.addLayout(self.user_key_layout)
+            self.qh.addLayout(self.user_value_layout)
+
+            for k, v in value.items():
+                self.user_key_label = QLabel(k)
+                self.user_key_label.setStyleSheet("max-height: 20px; font-size: 17px; max-width: 350px;")
+                self.user_key_layout.addWidget(self.user_key_label)
+
+                self.user_value_label = QLabel(v)
+                self.user_value_label.setStyleSheet("max-height: 20px; font-size: 17px; max-width: 350px;")
+                self.user_value_layout.addWidget(self.user_value_label)
+
+        self.tab7.setLayout(self.layout)
 
     def show_time(self):
         self.layout = QVBoxLayout()
@@ -135,23 +199,31 @@ class MyWidget(QMainWindow):
 
     def draw_disk_info_row(self, position_x, position_y, value, layout):
         text_edit = QTextEdit(value)
-        text_edit.setStyleSheet("font-size: 17px; max-height: 30px;")
+        text_edit.setStyleSheet("font-size: 17px; max-height: 30px; max-width: 150px;")
         layout.addWidget(text_edit, position_x, position_y)
 
     def show_disk_form(self, layout):
         string_number = 1
-        for disk in get_all_devices():
-            label = QLabel(disk)
-            label.setStyleSheet("font-size: 20px; max-height: 30px;")
-            layout.addWidget(label, string_number, 1)
-            self.draw_disk_info_row(string_number, 2, str(get_devices_space().get(disk).get("total")), layout)
-            self.draw_disk_info_row(string_number, 3, str(get_devices_space().get(disk).get("used")), layout)
-            self.draw_disk_info_row(string_number, 4, str(get_devices_space().get(disk).get("free")), layout)
-            self.draw_disk_info_row(string_number, 5, str(get_devices_space().get(disk).get("percent")), layout)
-            self.draw_disk_info_row(string_number, 6, str(get_devices_file_system().get(disk)), layout)
-            string_number += 1
+        try:
+            for disk in get_all_devices():
+                label = QLabel(disk)
+                label.setStyleSheet("font-size: 20px; max-height: 30px; max-width: 50px;")
+                layout.addWidget(label, string_number, 1)
+                self.draw_disk_info_row(string_number, 2, str(get_devices_space().get(disk).get("total")), layout)
+                self.draw_disk_info_row(string_number, 3, str(get_devices_space().get(disk).get("used")), layout)
+                self.draw_disk_info_row(string_number, 4, str(get_devices_space().get(disk).get("free")), layout)
+                self.draw_disk_info_row(string_number, 5, str(get_devices_space().get(disk).get("percent")), layout)
+                self.draw_disk_info_row(string_number, 6, str(get_devices_file_system().get(disk)), layout)
+                string_number += 1
+        except:
+            for disk in get_all_devices():
+                label = QLabel(disk)
+                label.setStyleSheet("font-size: 20px; max-height: 30px; max-width: 50px;")
+                layout.addWidget(label, string_number, 1)
+                self.draw_disk_info_row(string_number, 2, "Access Denied", layout)
+                string_number += 1
         last_label = QLabel()
-        layout.addWidget(last_label, string_number, 1)
+        layout.addWidget(last_label, string_number, 7)
 
 
 def main():
@@ -161,7 +233,12 @@ def main():
     timer = QTimer()
     timer.timeout.connect(ex.update_time)
     timer.start(100)
-    sys.exit(app.exec_())
+    # sys.exit(app.exec_())
+
+    try:
+        sys.exit(app.exec_())
+    except:
+        print("Exiting")
 
 
 if __name__ == '__main__':
